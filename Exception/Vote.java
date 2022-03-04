cd9f6209d4237992129b2785b2f7a6da6a3d1f49
Apsara Bishwokarma <apsarabishwokarma7@gmail.com>
2022-03-04 12:14:26 +0545

    java-codes

---
+++ Exception/Vote.java
@@ -0,0 +1,36 @@
+class InvalidvoteException extends InvalidvoteException{
+    public InvalidvoteException( String Msg)
+    {
+        super(msg);
+
+    }
+}
+class Vote
+{
+    String name;
+    int age;
+
+    public Vote(String name,int age)throws InvalidvoteException
+    {
+        if(age<18)
+        {
+            throw new InvalidvoteException("invalid!!");
+        }
+        else{
+            this.name=name;
+            this.age=age;
+
+        }
+    }
+    public static void main(String []args)
+    {
+        try{
+            Vote v= new Vote("sujata",15);
+
+        }
+        catch(InvalidvoteException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
